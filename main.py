df02148013d622970f2cf236368fe0337e40edbd
Mario Costa <cost.mario08@gmail.com>
2023-01-22 15:47:30 +0100

    added external library

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 import numpy as np
+from logzero import logger
 
 print("Hello world!")
 print(f"Today is {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
 
 x = np.array([[1,2,3],[4,5,6]])
 
-print(np.sqrt(x))
+logger.info(np.sqrt(x))
