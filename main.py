7d9c66e734b901f60ef14f8389a8065aa107fbd4
Emcy <88923647+LupusInFabulaStudios@users.noreply.github.com>
2023-01-22 15:41:38 +0100

    Added direct library

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from datetime import datetime
+import numpy as np
 
 print("Hello world!")
 print(f"Today is {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+
+x = np.array([[1,2,3],[4,5,6]])
+
+print(np.sqrt(x))
