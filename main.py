09c5254b0468ef4e7da1882ce5b6d646bdc9dc06
Mario Costa <88923647+LupusInFabulaStudios@users.noreply.github.com>
2023-01-24 22:42:43 +0100

    Take 3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import logzero
 from logzero import logger
 
-logzero.file("/boot/test.log")
+logzero.file("/tmp/logfile.log")
 
 logger.info("Hello world!")
 logger.info(f"Today is {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
